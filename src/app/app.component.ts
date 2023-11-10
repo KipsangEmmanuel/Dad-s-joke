e07bc2a5fd2b90261c9a38800914f94d2a7a55a8
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-11-10 05:51:21 -0800

    finalise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { JokesService } from './services//jokes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'dad-joke';
+  joke: string = '';
+  title = 'Stand Up jokes';
+  constructor(private jokeService: JokesService) { }  
+  fetchJoke(): void {
+    this.jokeService.getJoke().subscribe((data: any) => {
+      let joke=this.joke = data.joke;
+      console.log(joke);
+      
+    });
+  }
 }
